657c8cd08f19b0c5c739b69ad780720a662de8bd
Mihika Nadig <mn2825@columbia.edu>
2020-12-09 21:46:52 -0800

    Added more tests to test_dataset.py

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import pytest
+from kmeans import getCenters
+from kmeans import computeAlpha
+from kmeans import lloyds
+from kmeans import plot
 
 def test_dataset(cmdopt):
     check1 = False
@@ -11,4 +15,35 @@ def test_dataset(cmdopt):
     if len(df.columns) > 5:
         check2 = True
     if check1 != True or check2 != True:
-        pytest.fail("{}".format("Not a valid dataset"))
+       pytest.fail("{}".format("Not a valid dataset"))
+
+def test_center_inits():
+	data = [(1, 2), (3, 4), (5, 6), (7, 8), (9, 10)]
+	distances = [(0,0.1),(1,0.2), (2, 0.3), (3,0.4), (4, 0.5), (5, 0.6)]
+	centers, center_lists = getCenters(data, distances, 5)
+	if len(centers) != len(center_lists) or len(centers) != 4:
+		pytest.fail("{}".format("Centers and center lists are not initialized correctly."))
+
+def test_alpha_int():
+	data = None
+	labels = [0,1,2,3,1,2,4]
+	alphas = computeAlpha(data, labels)
+	if len(alphas) !=5:
+		pytest.fail("{}".format("Alphas list not initialized correctly."))
+	if alphas[0] != 1 or alphas[1] != 2 or alphas[2] != 2 or alphas[3] != 1 or alphas[4] != 1:
+		pytest.fail("{}".format("Incorrect alpha count."))
+
+def test_data_after_processing():
+	data = []
+	num = 5
+	res = lloyds(data, 5)
+	if res != None:
+		pytest.fail("{}".format("Empty data after preprocessing not detected."))
+
+def test_before_plot():
+	centers = [(1, 2), (3, 4), (5, 6), (7, 8), (9, 10)]
+	center_lists = [[],[(3, 4)], [(1, 2)], [(7, 8)], [(9, 10)]]
+	res = plot(centers, center_lists)
+	if res != None:
+		pytest.fail("{}".format("Error in cluster assignment not detected."))
+	
