55068a08e3cf088f09e5e0a36c33effa542e433f
Mihika Nadig <mn2825@columbia.edu>
2020-12-22 20:07:25 -0800

    Got rid of certain pylint checks

--- tests/test_lint.py
+++ tests/test_lint.py
@@ -3,11 +3,11 @@ from pylint import lint
 import pytest
 
 def test_lint():
-    THRESHOLD = 4
+    threshold = 5
     if len(sys.argv) < 2:
         raise ArgumentError("Fix input args")
-    run = lint.Run([sys.argv[2]], do_exit=False)
+    run = lint.Run(['--disable=C0103,W0621,C0200,R0912,R0915',sys.argv[2]], do_exit=False)
     score = run.linter.stats['global_note']
-
-    if score < THRESHOLD:
+    print("SCORE: ", score)
+    if score < threshold:
         pytest.fail("{}".format("Pylint score below threshold."))
